4eea365765fe5cf534875bfec8ef825af7a4d88f
Henry Lunn <hlunn88@gmail.com>
2022-12-01 12:52:07 +0000

    Updated completion calendar

--- src/Day01.hs
+++ src/Day01.hs
@@ -8,18 +8,18 @@ runDay01 p = case p of
   One -> part1
   Two -> part2
 
-elves' :: [String] -> [[String]]
-elves' xs = case break (== "") xs of
-    (a, _:b) -> a : elves' b
+parseInput' :: [String] -> [[String]]
+parseInput' xs = case break (== "") xs of
+    (a, _:b) -> a : parseInput' b
     (a, [])  -> [a]
 
-elves :: [String] -> [[Int]]
-elves xs = (map.map) (\x -> read x :: Int) $ elves' xs
+parseInput :: [String] -> [[Int]]
+parseInput xs = (map.map) (\x -> read x :: Int) $ parseInput' xs
 
 part1 :: [String] -> [String]
 part1 s = [show maxm] where
-    maxm = maximum $ map sum $ elves s
+    maxm = maximum $ map sum $ parseInput s
     
 part2 :: [String] -> [String]
 part2 s = [show top3] where
-    top3 = sum $ take 3 $ reverse $ sort $ map sum $ elves s
+    top3 = sum $ take 3 $ reverse $ sort $ map sum $ parseInput s
