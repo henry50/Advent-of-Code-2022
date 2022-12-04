b10ac1e8f0d4f6c7db5dcdf906f46ece98705966
Henry Lunn <hlunn88@gmail.com>
2022-12-04 06:21:45 +0000

    Day 4 complete

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,8 +2,35 @@ module Day04 (runDay04) where
 
 import Part ( Part(..) )
 
+import Data.List.Split ( splitOn )
+
 
 runDay04 :: Part -> [String] -> [String]
-runDay04 p = case p of
-  One -> error "Not implemented"
-  Two -> error "Not implemented"
+runDay04 p s = case p of
+  One -> [show $ solve s' fullyContains]
+  Two -> [show $ solve s' anyContains]
+  where s' = parseInput s
+
+solve :: [((Int, Int), (Int, Int))] -> ((Int, Int) -> (Int, Int) -> Bool) -> Int
+solve s f = length.filter (== True) $ map (uncurry f) s
+
+fullyContains :: (Int, Int) -> (Int, Int) -> Bool
+fullyContains (a, b) (c, d) = ((c >= a) && (d <= b)) || ((a >= c) && (b <= d))
+
+anyContains :: (Int, Int) -> (Int, Int) -> Bool
+anyContains (a, b) (c, d) = ((a <= c) && (c <= b)) ||
+                            ((a <= d) && (d <= b)) ||
+                            ((c <= a) && (a <= d)) ||
+                            ((c <= b) && (b <= d))
+
+parseInput :: [String] -> [((Int, Int), (Int, Int))]
+parseInput = map splitInput where
+  listToPair :: [a] -> (a,a)
+  listToPair [a,b] = (a,b)
+  listToPair _     = error "Not a pair"
+  rangeToPair :: String -> (Int, Int)
+  rangeToPair x = (read a, read b) where
+    (a, b) = listToPair $ splitOn "-" x
+  splitInput :: String -> ((Int, Int), (Int, Int))
+  splitInput x = (rangeToPair a, rangeToPair b) where
+    (a, b) = listToPair $ splitOn "," x
